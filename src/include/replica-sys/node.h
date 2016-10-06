cfc6dd0eecf9c2e68a541a9aba2866bdf921c000
Your Name <you@example.com>
2016-10-06 19:14:13 +0800

    delete replica

--- src/include/replica-sys/node.h
+++
@@ -1,8 +0,0 @@
-#ifndef NODE_H
-#define NODE_H
-#include "./replica.h"
-
-typedef struct node_t{
-}node;
-
-#endif
--- src/include/replica-sys/replica.h
+++
@@ -1,9 +0,0 @@
-#ifndef REPLICA_H
-#define REPLICA_H
-#include <sys/types.h>
-#include <stdint.h>
-
-struct node_t;
-struct node_t* system_initialize();
-
-#endif
