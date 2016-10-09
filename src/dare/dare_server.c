e24b73e5f6e93727945fe589df305cb2b2bc437a
Your Name <you@example.com>
2016-10-09 20:56:56 +0800

    nits

--- src/dare/dare_server.c
+++ src/dare/dare_server.c
@@ -1782,10 +1782,12 @@ apply_entry:
             //else {
             //    if (SID_GET_TERM(data.ctrl_data->sid) < 50) sleep(1);
             //}
-            if (!IS_LEADER)
+            data.last_cmt_csm_idx++;
+            if (!IS_LEADER) {
+            	data.last_csm_idx = data.last_cmt_csm_idx;
                 data.apply_cmd(entry->clt_id, entry->type, entry->data.cmd.len, &entry->data.cmd.cmd, data.up_para);
-            else
-                data.last_cmt_csm_idx++;
+            }
+                
             last_applied_entry.idx = entry->idx;
             last_applied_entry.term = entry->term;
             last_applied_entry.offset = data.log->apply;
