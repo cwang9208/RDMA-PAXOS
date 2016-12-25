c8959849da4415e5d23da399a7bc92dabe21a3f4
Wang Cheng <u3003676@connect.hku.hk>
2016-12-26 00:21:10 +0800

    Update config-proxy.c

--- src/config-comp/config-proxy.c
+++ src/config-comp/config-proxy.c
@@ -10,43 +10,11 @@ int proxy_read_config(struct proxy_node_t* cur_node,const char* config_path){
     if(!config_read_file(&config_file,config_path)){
         goto goto_config_error;
     }
-    
-    uint8_t group_size;
-    if(!config_lookup_int(&config_file,"group_size",(int*)&group_size)){
-        goto goto_config_error;
-    }
 
-    if(group_size<=cur_node->node_id){
-        err_log("PROXY : Invalid Node Id\n");
-        goto goto_config_error;
-    }
-    cur_node->group_size = group_size;
-
-// parse proxy address
-
-    config_setting_t *proxy_config = NULL;
-    proxy_config = config_lookup(&config_file,"proxy_config");
-
-    if(NULL==proxy_config){
-        err_log("PROXY : Cannot Find Nodes Settings.\n");
-        goto goto_config_error;
-    }    
-
-    config_setting_t *pro_ele = config_setting_get_elem(proxy_config,cur_node->node_id);
-
-//    err_log("PROXY : Current Node Id Is %u.\n",cur_node->node_id);
-
-    if(NULL==pro_ele){
-        err_log("PROXY : Cannot Find Current Node's Address Section.\n");
-        goto goto_config_error;
-    }
-
-// read the option for log, if it has some sections
-    
-    config_setting_lookup_int(pro_ele,"req_log",&cur_node->req_log);
+    config_lookup_int(&config_file,"req_log",&cur_node->req_log);
 
     const char* db_name;
-    if(!config_setting_lookup_string(pro_ele,"db_name",&db_name)){
+    if(!config_lookup_string(&config_file,"db_name",&db_name)){
         goto goto_config_error;
     }
     size_t db_name_len = strlen(db_name);
@@ -60,32 +28,13 @@ int proxy_read_config(struct proxy_node_t* cur_node,const char* config_path){
     }
     cur_node->db_name[db_name_len] = '\0';
 
-// parse server address
-
-    config_setting_t *server_config;
-    server_config = config_lookup(&config_file,"server_config");
-
-    if(NULL==server_config){
-        err_log("cannot find nodes settings \n");
-        goto goto_config_error;
-    }    
-
-    if(NULL==server_config){
-        err_log("cannot find node address section \n");
-        goto goto_config_error;
-    }
-    config_setting_t *serv_ele = config_setting_get_elem(server_config,cur_node->node_id);
-    if(NULL==serv_ele){
-        err_log("cannot find current node's address\n");
-        goto goto_config_error;
-    }
 
     const char* peer_ipaddr=NULL;
     int peer_port=-1;
-    if(!config_setting_lookup_string(serv_ele,"ip_address",&peer_ipaddr)){
+    if(!config_lookup_string(&config_file,"ip_address",&peer_ipaddr)){
         goto goto_config_error;
     }
-    if(!config_setting_lookup_int(serv_ele,"port",&peer_port)){
+    if(!config_lookup_int(&config_file,"port",&peer_port)){
         goto goto_config_error;
     }
 
