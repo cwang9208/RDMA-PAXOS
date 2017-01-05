3a8e619cb8abe349d9c456f1cab1a3a26c8614e0
Your Name <you@example.com>
2017-01-05 17:39:02 +0800

    changed to socket for safety

--- src/config-comp/config-proxy.c
+++ src/config-comp/config-proxy.c
@@ -11,12 +11,16 @@ int proxy_read_config(struct proxy_node_t* cur_node,const char* config_path){
         goto goto_config_error;
     }
 
-    config_lookup_int(&config_file,"req_log",&cur_node->req_log);
-
+    config_setting_t *proxy_global_config;
+    proxy_global_config = config_lookup(&config_file,"proxy_global_config");
     const char* db_name;
-    if(!config_lookup_string(&config_file,"db_name",&db_name)){
+    if(!config_setting_lookup_string(proxy_global_config,"db_name",&db_name)){
         goto goto_config_error;
     }
+    if(!config_setting_lookup_int(proxy_global_config,"req_log",&cur_node->req_log)){
+        goto goto_config_error;
+    }
+
     size_t db_name_len = strlen(db_name);
     cur_node->db_name = (char*)malloc(sizeof(char)*(db_name_len+1));
     if(cur_node->db_name==NULL){
@@ -28,28 +32,30 @@ int proxy_read_config(struct proxy_node_t* cur_node,const char* config_path){
     }
     cur_node->db_name[db_name_len] = '\0';
 
-// parse server address
     const char* peer_ipaddr=NULL;
     int peer_port=-1;
-    if(!config_lookup_string(&config_file,"ip_address",&peer_ipaddr)){
+// parse server address
+    config_setting_t *server_global_config;
+    server_global_config = config_lookup(&config_file,"server_global_config");
+
+    if(!config_setting_lookup_string(server_global_config,"ip_address",&peer_ipaddr)){
         goto goto_config_error;
     }
-    if(!config_lookup_int(&config_file,"port",&peer_port)){
+    if(!config_setting_lookup_int(server_global_config,"port",&peer_port)){
         goto goto_config_error;
     }
 
     cur_node->sys_addr.s_addr.sin_port = htons(peer_port);
     cur_node->sys_addr.s_addr.sin_family = AF_INET;
     inet_pton(AF_INET,peer_ipaddr,&cur_node->sys_addr.s_addr.sin_addr);
-
     cur_node->sys_addr.s_sock_len = sizeof(cur_node->sys_addr.s_addr);
 
 
 // parse consensus address
-    config_setting_t *consensus_config;
-    consensus_config = config_lookup(&config_file,"consensus_config");
+    config_setting_t *consensus_global_config;
+    consensus_global_config = config_lookup(&config_file,"consensus_global_config");
 
-    if(NULL==consensus_config){
+    if(NULL==consensus_global_config){
         err_log("PROXY : Cannot Find Nodes Settings \n");
         goto goto_config_error;
     }    
@@ -57,10 +63,10 @@ int proxy_read_config(struct proxy_node_t* cur_node,const char* config_path){
     peer_ipaddr=NULL;
     peer_port=-1;
 
-    if(!config_setting_lookup_string(consensus_config,"ip_address",&peer_ipaddr)){
+    if(!config_setting_lookup_string(consensus_global_config,"ip_address",&peer_ipaddr)){
         goto goto_config_error;
     }
-    if(!config_setting_lookup_int(consensus_config,"port",&peer_port)){
+    if(!config_setting_lookup_int(consensus_global_config,"port",&peer_port)){
         goto goto_config_error;
     }
     cur_node->sys_addr.c_addr.sin_port = htons(peer_port);
